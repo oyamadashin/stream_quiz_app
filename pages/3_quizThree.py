045e50d8460242626127bc88e42b6f760bffe8dc
Shin Oyamada <oyamadashin@gmail.com>
2025-04-12 14:26:04 +0900

    最初のコミット

--- pages/3_quizThree.py
+++ pages/3_quizThree.py
@@ -11,6 +11,9 @@ if "score" not in st.session_state:
 if "answered" not in st.session_state:
     st.session_state.answered = False
 
+# 画面を左、中央、右に1:2:1に分割
+left, center, right = st.columns([1, 2, 1])
+
 # csv読み込み
 with open("resources/quiz_questions.csv", encoding="UTF-8") as f:
     reader = csv.DictReader(f)
@@ -32,43 +35,43 @@ quiz = st.session_state.quiz
 choices = st.session_state.choices
 
 
+with center:
+    st.title("❓問題3（上級編）")
+    # TODO:画像はあとで
+    # st.image(f"static/images/{quiz['image']}.png", caption = quiz["question"], width = 400)
+    st.subheader(quiz["question"])
 
-st.title("❓問題3（上級編）")
-# TODO:画像はあとで
-# st.image(f"static/images/{quiz['image']}.png", caption = quiz["question"], width = 400)
-st.subheader(quiz["question"])
-
-# ラジオボタンで選択
-answer = st.radio(
-    "選んでください",
-    choices
-)
-
-
-# ボタンで答える
-if st.button("解答する") and not st.session_state.answered:
-    st.session_state.answered = True #回答済みにする
-    if answer == quiz["answer"]:
-        st.success("正解！")
-        st.session_state.score += 1
-    else:
-        st.error("不正解...")
-
-if st.session_state.get("answered"):
-    if st.button("➡ 次のクイズへ"):
-        del st.session_state.quiz
-        del st.session_state.choices
-        st.session_state.answered = False  # 回答状態をリセット
-        st.switch_page("pages/4_結果表示.py")  # 結果ページへ遷移
+    # ラジオボタンで選択
+    answer = st.radio(
+        "選んでください",
+        choices
+    )
 
 
+    # ボタンで答える
+    if st.button("解答する") and not st.session_state.answered:
+        st.session_state.answered = True #回答済みにする
+        if answer == quiz["answer"]:
+            st.success("正解！")
+            st.session_state.score += 1
+        else:
+            st.error("不正解...")
 
-st.markdown("---")  # 横線で区切る
+    if st.session_state.get("answered"):
+        if st.button("➡ 次のクイズへ"):
+            del st.session_state.quiz
+            del st.session_state.choices
+            st.session_state.answered = False  # 回答状態をリセット
+            st.switch_page("pages/4_結果表示.py")  # 結果ページへ遷移
 
-st.subheader("クイズをやめて最初に戻りますか？")  # 見出しをつける
 
-# 最初に戻る用
-if st.button("🏠️最初に戻る"):
-    st.session_state.score = 0 # スコアをリセット
-    st.session_state.answered = False # 回答状態をリセット（念のため）
-    st.switch_page("quiz_demo.py") # トップページへ遷移
+
+    st.markdown("---")  # 横線で区切る
+
+    st.subheader("クイズをやめて最初に戻りますか？")  # 見出しをつける
+
+    # 最初に戻る用
+    if st.button("🏠️最初に戻る"):
+        st.session_state.score = 0 # スコアをリセット
+        st.session_state.answered = False # 回答状態をリセット（念のため）
+        st.switch_page("app.py") # トップページへ遷移
